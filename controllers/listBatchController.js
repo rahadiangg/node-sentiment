7f919446e223812d9c1d77d5f189c80b43d9d85e
rahadiangg <dianraha11@gmail.com>
2022-02-04 11:59:47 +0700

    add persentase to list batch

--- controllers/listBatchController.js
+++ controllers/listBatchController.js
@@ -1,4 +1,5 @@
 const models = require('../models/index.js');
+const {Op} = require('sequelize');
 
 exports.listBatch = async (req, res) => {
     const data = await models.batchs.findAll({
@@ -6,6 +7,48 @@ exports.listBatch = async (req, res) => {
             ["createdAt", "DESC"]
         ],
     });
+
+    let count = 0;
+    for (let d of data) {
+        let total = await models.sentiments.count({
+            where: {
+                batch_id: d.id
+            }
+        });
+
+        let positive = await models.sentiments.count({
+            where: {
+                batch_id: d.id,
+                score: {
+                    [Op.lt]: 0
+                }
+            }
+        });
+
+        let negatif = await models.sentiments.count({
+            where: {
+                batch_id: d.id,
+                score: {
+                    [Op.gt]: 0
+                }
+            }
+        });
+
+        let netral = await models.sentiments.count({
+            where: {
+                batch_id: d.id,
+                score: {
+                    [Op.eq]: 0
+                }
+            }
+        });
+
+        data[count].persentase_positive = (positive / total) * 100;
+        data[count].persentase_negative = (negatif / total) * 100;
+        data[count].persentase_netral = (netral / total) * 100;
+
+        count++;
+    }
     
     res.render("pages/listBatch" ,{
         layout: 'layouts/main',
